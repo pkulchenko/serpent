7ff75bebd7d6bd71fa7767fb234aa41109a073c2
Paul Kulchenko <paul@kulchenko.com>
2015-01-19 21:54:46 -0800

    Updated copyright year.

--- src/serpent.lua
+++ src/serpent.lua
@@ -1,4 +1,4 @@
-local n, v = "serpent", 0.273 -- (C) 2012-13 Paul Kulchenko; MIT License
+local n, v = "serpent", 0.273 -- (C) 2012-15 Paul Kulchenko; MIT License
 local c, d = "Paul Kulchenko", "Lua serializer and pretty printer"
 local snum = {[tostring(1/0)]='1/0 --[[math.huge]]',[tostring(-1/0)]='-1/0 --[[-math.huge]]',[tostring(0/0)]='0/0'}
 local badtype = {thread = true, userdata = true, cdata = true}
