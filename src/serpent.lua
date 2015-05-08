dca4b8c1d8db202db93ddfd3cfdff17d3a5821d6
Paul Kulchenko <paul@kulchenko.com>
2015-05-07 22:42:24 -0700

    Added using `debug.metatable` when available.

--- src/serpent.lua
+++ src/serpent.lua
@@ -1,7 +1,8 @@
-local n, v = "serpent", 0.281 -- (C) 2012-15 Paul Kulchenko; MIT License
+local n, v = "serpent", 0.282 -- (C) 2012-15 Paul Kulchenko; MIT License
 local c, d = "Paul Kulchenko", "Lua serializer and pretty printer"
 local snum = {[tostring(1/0)]='1/0 --[[math.huge]]',[tostring(-1/0)]='-1/0 --[[-math.huge]]',[tostring(0/0)]='0/0'}
 local badtype = {thread = true, userdata = true, cdata = true}
+local getmetatable = debug and debug.getmetatable or getmetatable
 local keyword, globals, G = {}, {}, (_G or _ENV)
 for _,k in ipairs({'and', 'break', 'do', 'else', 'elseif', 'end', 'false',
   'for', 'function', 'goto', 'if', 'in', 'local', 'nil', 'not', 'or', 'repeat',
